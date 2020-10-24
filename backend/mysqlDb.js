da8b34e469db02dfcb1abbf6112ff79d12d958f6
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-10-24 17:22:42 +0600

    Добавил обработку запросов на добавление и удаление

--- backend/mysqlDb.js
+++ backend/mysqlDb.js
@@ -1,18 +1,38 @@
 module.exports = db => ({
-    getItems(resource) {
+    getItems(resource, news_id) {
         return new Promise((res, rej) => {
-            db.query("SELECT id, title, image, date FROM ??", [resource], (err, result) => {
-                if(err) {
-                    rej(err);
+            if(resource === 'news') {
+                db.query("SELECT id, title, image, date FROM ??", [resource], (err, result) => {
+                    if(err) {
+                        rej(err);
+                    } else {
+                        res(result);
+                    }
+                });
+            } else {
+                if(news_id) {
+                    db.query("SELECT * FROM ?? WHERE news_id = ?", [resource, news_id], (err, result) => {
+                        if(err) {
+                            rej(err);
+                        } else {
+                            res(result);
+                        }
+                    });
                 } else {
-                    res(result);
+                    db.query("SELECT * FROM ??", [resource], (err, result) => {
+                       if(err) {
+                           rej(err);
+                       } else {
+                           res(result);
+                       }
+                    });
                 }
-            });
+            }
         })
     },
     getItem(resource, id) {
         return new Promise((res, rej) => {
-            db.query("SELECT * FROM ??", [resource, id], (err, result) => {
+            db.query("SELECT * FROM ?? WHERE id = ?", [resource, id], (err, result) => {
                 if(err) {
                     rej(err);
                 } else {
@@ -20,5 +40,32 @@ module.exports = db => ({
                 }
             });
         });
-    }
+    },
+    createItem(resource, item) {
+        return new Promise((res, rej) => {
+            db.query("INSERT INTO ?? SET ?", [resource, item], (err, result) => {
+                if(err) {
+                    rej(err);
+                }
+                item.id = result.insertId;
+                db.query("SELECT * FROM ?? WHERE id = ?", [resource, item.id], (err, result) => {
+                    if(err) {
+                        rej(err);
+                    } else {
+                        res(result);
+                    }
+                });
+            });
+        });
+    },
+    deleteItem(resource, id) {
+        return new Promise((res, rej) => {
+            db.query("DELETE FROM ?? WHERE id = ?", [resource, id], (err) => {
+                if(err) {
+                    rej(err);
+                }
+                res({successMessage: `Item with id: ${id} has been removed from table ${resource}`});
+            });
+        })
+    },
 });
