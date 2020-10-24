2006d1b4b8c74a7689f58d47ee1d14e07372edbb
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-10-24 17:26:52 +0600

    Добавил в навигацию страницу с добавлением новостей

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Layout from "./components/Layout/Layout";
 import Main from "./containers/Main/Main";
 import {Route, Switch} from "react-router-dom";
 import SingleNews from "./containers/SingleNews/SingleNews";
+import AddNewNews from "./containers/AddNewNews/AddNewNews";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Main} />
         <Route path="/news" exact component={Main} />
+        <Route path="/add-news" component={AddNewNews} />
         <Route path="/news/:id" exact component={SingleNews} />
         <Route render={() => <h1 style={{textAlign: 'center'}}>404 not found</h1>} />
       </Switch>
