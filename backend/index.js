28fedf76533537af6b69e7e28980ba9095f0f15f
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-10-24 12:10:39 +0600

    Главный файл с настройками сервера

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const config = require('./config');
+const db = require('./mysqlDb');
+const news = require('./app/news');
+
+const app = express();
+const PORT = 8000;
+
+const connection = mysql.createConnection(config.database);
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+connection.connect(err => {
+    if(err) {
+        console.log(err);
+        throw err;
+    }
+    console.log('MySQL Database is connected');
+    app.use('/news', news(db(connection)));
+    app.use((req, res) => {
+        res.status(404).send({"error": "404 Not found"});
+    });
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+})
