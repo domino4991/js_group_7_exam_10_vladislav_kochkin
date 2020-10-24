512b55a634e208e2bf5c288a0214e8a9950ba737
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-10-24 13:41:40 +0600

    Роуты для навигации

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import './App.css';
+import Layout from "./components/Layout/Layout";
+import Main from "./containers/Main/Main";
+import {Route, Switch} from "react-router-dom";
+import SingleNews from "./containers/SingleNews/SingleNews";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/news" exact component={Main} />
+        <Route path="/news/:id" exact component={SingleNews} />
+        <Route render={() => <h1 style={{textAlign: 'center'}}>404 not found</h1>} />
+      </Switch>
+    </Layout>
   );
 }
 
